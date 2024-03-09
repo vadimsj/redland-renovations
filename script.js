26158273d01f78866d1e0b341bdd2fdb0f321619
Vadims <vad773@pm.me>
2024-03-09 17:28:00 +0000

    feat: add function to format phone numbers

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 /* Function to add a shadow to the nav bar when the header is scrolled */
-
 document.addEventListener('DOMContentLoaded', function() {
-    var header = document.querySelector('.header');
+    let header = document.querySelector('.header');
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 0) {
@@ -14,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /* Function to display the content on the page as it enters into the viewport */
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -28,3 +26,38 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* Function to auto format mobile phone numbers to start with 44 */
+function formatPhoneNumber() {
+    let phoneNumberInput = document.getElementById("telephone");
+    let phoneNumberValue = phoneNumberInput.value;
+
+    // Remove any non-digit characters
+    let formattedNumber = phoneNumberValue.replace(/\D/g, '');
+
+    // Check if the phone number starts with "07"
+    if (formattedNumber.startsWith("07")) {
+      // Replace "07" with "+44"
+      formattedNumber = formattedNumber.replace(/^07/, "+44");
+    }
+
+    // Update the input field value
+    phoneNumberInput.value = formattedNumber;
+  }
+
+  /* Function to set either phone number or email to be required */
+  function toggleRequiredFields() {
+    let emailInput = document.getElementById("email");
+    let phoneInput = document.getElementById("telephone");
+  
+    if (phoneInput.value.trim() !== "") {
+      emailInput.removeAttribute("required");
+      phoneInput.setAttribute("required", "required");
+    } else if (emailInput.value.trim() !== "") {
+      phoneInput.removeAttribute("required");
+      emailInput.setAttribute("required", "required");
+    } else {
+      emailInput.setAttribute("required", "required");
+      phoneInput.setAttribute("required", "required");
+    }
+  }
