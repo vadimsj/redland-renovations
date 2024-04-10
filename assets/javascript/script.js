63f929918cf45c40b3fab50fda310ea043e6b51d
Vadims <vad773@pm.me>
2024-04-10 14:01:36 +0100

    style: correct comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -131,7 +131,7 @@ function hideOnScroll(selector) {
         const elementTop = bounding.top + window.scrollY;
         const elementBottom = elementTop + bounding.height;
 
-        // Calculate the threshold for fading out the element when it's 60% out of the viewport
+        // Calculate the threshold for fading out the element when it's 65% out of the viewport
         const threshold = viewportBottom - ((viewportBottom - viewportTop) * 0.65);
 
         // Check if the element is below the threshold
